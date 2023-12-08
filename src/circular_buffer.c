d967b0d5fcba627e9cb665c490d85c83ec480f07
Kristijan Todorovic <e11806442@student.tuwien.ac.at>
2023-12-08 01:16:41 +0100

    first ideas of circular buffer implementation

--- src/circular_buffer.c
+++ src/circular_buffer.c
@@ -1,5 +1,58 @@
-//
-// Created by Kristijan Todorovic on 28.11.23.
-//
+/**
+ * @file circular_buffer.h
+ * @author Kristijan Todorovic, [e11806442(at)student.tuwien.ac.at]
+ * @date 28.11.2023
+ */
 
 #include "circular_buffer.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+static void err_exit(char *message) {
+    fprintf(stderr, "%s\n", message);
+    exit(EXIT_FAILURE);
+}
+
+circular_buffer circular_buffer_constr(void) {
+    circular_buffer cb;
+
+    cb.wr_pos = 0;
+    cb.rd_pos = 0;
+
+    cb.sem_free = sem_open(SEM_FREE, O_CREAT | O_EXCL, 0600, MAX_DATA);
+    if (cb.sem_free == SEM_FAILED) {
+        err_exit("error opening SEM_FREE");
+    }
+    cb.sem_used = sem_open(SEM_USED, O_CREAT | O_EXCL, 0600, 0);
+    if (cb.sem_used == SEM_FAILED) {
+        err_exit("error opening SEM_USED");
+    }
+    cb.sem_mutex = sem_open(SEM_MUTEX, O_CREAT | O_EXCL, 0600, 1);
+    if (cb.sem_mutex == SEM_FAILED) {
+        err_exit("error opening semaphore");
+    }
+
+    return cb;
+}
+
+void circular_buffer_write(circular_buffer *cb, graph *g) {
+    sem_wait(cb->sem_free);
+    sem_wait(cb->sem_mutex);
+    cb->data[cb->wr_pos] = *g;
+    sem_post(cb->sem_mutex);
+    sem_post(cb->sem_used);
+    cb->wr_pos++;
+    cb->wr_pos %= MAX_DATA;
+}
+
+graph *circular_buffer_read(circular_buffer *cb) {
+    sem_wait(cb->sem_used);
+    graph *g = &cb->data[cb->rd_pos];
+    sem_post(cb->sem_free);
+    cb->rd_pos++;
+    cb->rd_pos %= MAX_DATA;
+
+    return g;
+}
+
--- src/circular_buffer.h
+++ src/circular_buffer.h
@@ -1,11 +1,36 @@
 /**
  * @file circular_buffer.h
- * @author Kristijan Todorovic <e11806442@student.tuwien.ac.at>
+ * @author Kristijan Todorovic, [e11806442(at)student.tuwien.ac.at]
  * @date 28.11.2023
  */
 
 #ifndef CIRCULAR_BUFFER_H
 #define CIRCULAR_BUFFER_H
+
+#include <semaphore.h>
+#include "graph_util.h"
+
+#define SHM_NAME "/11806442_shm"
+#define MAX_DATA (32)
+
+#define SEM_FREE "/11806442_sem_free"
+#define SEM_USED "/11806442_sem_used"
+#define SEM_MUTEX "/11806442_sem_mutex"
+
+typedef struct circular_buffer {
+    int wr_pos;
+    int rd_pos;
+    graph data[MAX_DATA];
+    sem_t *sem_free;
+    sem_t *sem_used;
+    sem_t *sem_mutex;
+} circular_buffer;
+
+circular_buffer circular_buffer_constr(void);
+
+void circular_buffer_write(circular_buffer *cb, graph *g);
+
+graph *circular_buffer_read(circular_buffer *cb);
 
 #endif  //CIRCULAR_BUFFER_H
 
